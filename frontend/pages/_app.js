bd7bd6d32d1cb9f03ebfb7c074df4af5bbb83ebe
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2024-02-01 03:37:42 +0530

    feat: :sparkles: add Images, Logo, swr, api routes

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -9,6 +9,7 @@ import Footer from "@/components/Footer";
 import { Provider } from "react-redux";
 import store from "@/store/store";
 
+
 export default function App({ Component, pageProps }) {
 	return (
 		<>
