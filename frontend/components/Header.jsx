51e167659f0b618cfb3c9507eb9df5860a00b206
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2024-02-01 02:53:50 +0530

    feat: :sparkles: added hooks

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,10 +1,18 @@
 import React, {useState, useEffect} from "react";
-
+import Menu from "./Menu";
 import Link from "next/link";
 
 const Header = () => {
+    const [show, setShow] = useState("translate-y-0");
+    const [showCatMenu, setShowCatMenu] = useState(false);
+    const [categories, setCategories] = useState(null);
+    const [lastScrollY, setLastScrollY] = useState(0);
+    const [mobileMenu, setMobileMenu] = useState(false);
+
+    const {cartItems} = useSelector((state) => state.cart);
+
     return(
-        <header>
+        <header className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}>
             <span>header</span>
         </header>
     )
