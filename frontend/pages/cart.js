7375da90c311216b88ba73ac099854f9e9ec49c3
AvaterClasher <116944847+AvaterClasher@users.noreply.github.com>
2024-02-03 12:13:27 +0530

    feat: :sparkles: init backend

--- frontend/pages/cart.js
+++ frontend/pages/cart.js
@@ -43,17 +43,12 @@ const Cart = () => {
             <Wrapper>
                 {cartItems.length > 0 && (
                     <>
-                        {/* HEADING AND PARAGRAPH START */}
                         <div className="text-center max-w-[800px] mx-auto mt-8 md:mt-0">
                             <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
                                 Shopping Cart
                             </div>
                         </div>
-                        {/* HEADING AND PARAGRAPH END */}
-
-                        {/* CART CONTENT START */}
                         <div className="flex flex-col lg:flex-row gap-12 py-10">
-                            {/* CART ITEMS START */}
                             <div className="flex-[2]">
                                 <div className="text-lg font-bold">
                                     Cart Items
@@ -62,9 +57,6 @@ const Cart = () => {
                                     <CartItem key={item.id} data={item} />
                                 ))}
                             </div>
-                            {/* CART ITEMS END */}
-
-                            {/* SUMMARY START */}
                             <div className="flex-[1]">
                                 <div className="text-lg font-bold">Summary</div>
 
@@ -85,8 +77,6 @@ const Cart = () => {
                                         international transaction fees.
                                     </div>
                                 </div>
-
-                                {/* BUTTON START */}
                                 <button
                                     className="w-full py-4 rounded-full bg-black text-white text-lg font-medium transition-transform active:scale-95 mb-3 hover:opacity-75 flex items-center gap-2 justify-center"
                                     onClick={handlePayment}
@@ -94,15 +84,11 @@ const Cart = () => {
                                     Checkout
                                     {loading && <img src="/spinner.svg" />}
                                 </button>
-                                {/* BUTTON END */}
                             </div>
-                            {/* SUMMARY END */}
                         </div>
-                        {/* CART CONTENT END */}
                     </>
                 )}
 
-                {/* This is empty screen */}
                 {cartItems.length < 1 && (
                     <div className="flex-[2] flex flex-col items-center pb-[50px] md:-mt-14">
                         <Image
--- frontend/pages/category/[slug].js
+++ frontend/pages/category/[slug].js
@@ -85,7 +85,6 @@ export async function getStaticPaths() {
   };
 }
 
-// `getStaticPaths` requires using `getStaticProps`
 export async function getStaticProps({ params: { slug } }) {
   const category = await fetchDataFromApi(
     `/api/categories?filters[slug][$eq]=${slug}`
